3e59869929b59fe51560ed07b5bbc90386ad34ad
Chris <chris.mood92@gmail.com>
2019-05-15 14:20:54 -0500

    Fix nomenclature

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,8 +1,8 @@
-const para = document.querySelector('p')
+const pElement = document.querySelector('p')
 
-para.addEventListener('click', updateName)
+pElement.addEventListener('click', updateName)
 
 function updateName() {
   let name = prompt('Ingresa un nuevo nombre')
-  para.textContent = 'Player 1: ' + name
+  pElement.textContent = 'Player 1: ' + name
 }
